d80fafdc64b518cdd8ea71c6a8017d001899945a
Berke Aksoy <berkeaksoj@gmail.com>
2023-12-02 01:44:45 +0100

    New Input System Installed

--- Assets/_Scripts/Player/CharacterJumpController.cs
+++ Assets/_Scripts/Player/CharacterJumpController.cs
@@ -7,5 +7,23 @@ namespace BerkeAksoyCode
     public class CharacterJumpController : MonoBehaviour
     {
 
+        private bool jumpPressed, jumpReleased;
+
+        private void Awake()
+        {
+            
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space)){
+                jumpPressed = true;
+            }
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                jumpReleased = true;
+            }
+        }
+
     }
 }
