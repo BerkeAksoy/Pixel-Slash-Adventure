64183d3632f70b84a483d051f8b5f5ab95c8c832
Berke Aksoy <berkeaksoj@gmail.com>
2023-12-02 01:28:29 +0100

    Fix

---
+++ Assets/_Scripts/Player/CharacterJumpController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BerkeAksoyCode
+{
+    public class CharacterJumpController : MonoBehaviour
+    {
+
+    }
+}
